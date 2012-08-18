2cef3e5f77bfee77c87fe80df1798ee419a1c538
tomykaira <tomykaira@gmail.com>
2012-08-18 14:27:13 +0900

    Define has_one relationship with DSL

--- lib/rails_launcher/dsl.rb
+++ lib/rails_launcher/dsl.rb
@@ -23,19 +23,42 @@ module RailsLauncher
         m = Model.new(name)
         m.instance_eval(&block)
         @models << m
+
+        eigen_class = class << self; self; end
+        eigen_class.instance_eval do
+          define_method name do
+            m
+          end
+        end
       end
     end
 
     class Model
-      attr_reader :name, :fields
+      attr_reader :name, :fields, :relations
 
       def initialize(name)
         @name = name
         @fields = []
+        @relations = []
       end
 
       def string(name, opts = {})
         @fields << ['string', name]
+      end
+
+      # Add has_one relationship to the given model
+      #
+      def has_one(model)
+        @relations << ['has_one', model.name]
+        model.belongs_to(self)
+      end
+
+      # Add belongs_to relationship
+      # Do not use this function from DSL
+      # called by other models
+      #
+      def belongs_to(model)
+        @relations << ['belongs_to', model.name]
       end
     end
   end
--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -10,4 +10,27 @@ describe RailsLauncher::DSL do
       expect(subject.models.first.fields.first).to eq(['string', 'user_name'])
     end
   end
+
+  describe 'two models with has_one relationship' do
+    subject(:world) do
+      RailsLauncher::DSL.new_world_block do
+        model(:user) { string 'user_name' }
+        model(:blog) { string 'title' }
+        user.has_one blog
+      end
+    end
+
+    it { should have(2).models }
+    specify 'user model should have one blog' do
+      expect(model(:user).relations.first).to eq(['has_one', :blog])
+    end
+
+    specify 'blog model should belong to an user' do
+      expect(model(:blog).relations.first).to eq(['belongs_to', :user])
+    end
+
+    def model(name)
+      world.models.find { |m| m.name == name }
+    end
+  end
 end
