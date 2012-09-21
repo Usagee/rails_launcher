70a39bad19f9923b570269755ca772629a85a087
tomykaira <tomykaira@gmail.com>
2012-09-21 21:31:43 +0900

    Set subject from describe clause

--- spec/plugins/devise_spec.rb
+++ spec/plugins/devise_spec.rb
@@ -17,6 +17,8 @@ module RailsLauncher
     let(:devise_path) { File.expand_path(File.join(__FILE__, '../../../lib/rails_launcher/plugins/devise.rb')) }
     let(:constructor) { FileConstructor.new(world) }
 
+    subject(:file) { content_of_file(example.metadata[:example_group][:description]) }
+
     describe 'common files' do
       let(:world) { DSL.new_world %Q{
 plugin '#{ devise_path }', database_authenticatable: true
@@ -34,17 +36,14 @@ plugin '#{ devise_path }', database_authenticatable: true
 }}
 
       describe 'app/models/user.rb' do
-        subject(:file) { content_of_file('app/models/user.rb') }
         it { should match_line 'devise :database_authenticatable' }
       end
 
       describe 'config/routes.rb' do
-        subject(:file) { content_of_file('config/routes.rb') }
         it { should match_line 'devise_for :users' }
       end
 
-      describe 'db/migrate/xxx_devise_create_users.rb' do
-        subject(:file) { content_of_file('db/migrate/\d*_devise_create_users.rb') }
+      describe 'db/migrate/\d+_devise_create_users.rb' do
         it { should match_line 't.string :email, :null => false, :default => ""' }
         it { should match_line 't.string :encrypted_password, :null => false, :default => ""' }
         it { should match_line 'add_index :users, :email, :unique => true' }
@@ -58,20 +57,17 @@ plugin '#{ devise_path }', database_authenticatable: true, omniauthable: [:twitt
 }}
 
       describe 'app/models/user.rb' do
-        subject(:file) { content_of_file('app/models/user.rb') }
         it { should include ':omniauthable' }
         it { should include 'def self.find_for_oauth(auth, sign_in_resource = nil)' }
       end
 
-      describe 'db/migrate/xxx_devise_create_users.rb' do
-        subject(:file) { content_of_file('db/migrate/\d*_devise_create_users.rb') }
+      describe 'db/migrate/\d+_devise_create_users.rb' do
         it { should match_line 't.string :provider' }
         it { should match_line 't.string :uid' }
         it { should match_line 'add_index :users, :uid' }
       end
 
       describe 'config/initializers/devise.rb' do
-        subject(:file) { content_of_file('config/initializers/devise.rb') }
         it { should match_line 'require "omniauth-twitter"' }
         it { should match_line 'require "omniauth-facebook"' }
 
@@ -80,14 +76,10 @@ plugin '#{ devise_path }', database_authenticatable: true, omniauthable: [:twitt
       end
 
       describe 'config/routes.rb' do
-        subject(:file) { content_of_file('config/routes.rb') }
-
         it { should include_in_line 'devise_for', ':omniauth_callbacks => "users/omniauth_callbacks"' }
       end
 
       describe 'app/controllers/users/omniauth_callbacks_controller.rb' do
-        subject(:file) { content_of_file('app/controllers/users/omniauth_callbacks_controller.rb') }
-
         it { should include 'class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController' }
         it { should include 'def twitter' }
         it { should include 'def facebook' }
@@ -113,12 +105,10 @@ plugin '#{ devise_path }', database_authenticatable: true, rememberable: true
 }}
 
       describe 'app/models/user.rb' do
-        subject(:file) { content_of_file('app/models/user.rb') }
         it { should include_in_line 'attr_accessible', ':remember_me' }
       end
 
-      describe 'db/migrate/xxx_devise_create_users.rb' do
-        subject(:file) { content_of_file('db/migrate/\d*_devise_create_users.rb') }
+      describe 'db/migrate/\d*_devise_create_users.rb' do
         it { should match_line 't.datetime :remember_created_at' }
       end
     end
@@ -130,12 +120,10 @@ model(:user) { string :nickname }
 }}
 
       describe 'app/models/user.rb' do
-        subject(:file) { content_of_file('app/models/user.rb') }
         it { should include_in_line 'attr_accessible', ':nickname', ':email' }
       end
 
-      describe 'db/migrate/xxx_devise_create_users.rb' do
-        subject(:file) { content_of_file('db/migrate/\d*_devise_create_users.rb') }
+      describe 'db/migrate/\d*_devise_create_users.rb' do
         it { should match_line 't.string :nickname' }
       end
     end
